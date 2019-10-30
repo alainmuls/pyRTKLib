3e3c66c817fcdcb27f2134a8618d5d476832d0a4
Alain Muls <alain.muls@gmail.com>
2019-10-30 16:55:42 +0100

    python convention applied

--- TEST/ftp-download.py
+++ TEST/ftp-download.py
@@ -3,15 +3,15 @@
 from ftplib import FTP
 
 ftp = FTP('cddis.gsfc.nasa.gov')
-ftp.login('ftp','alain.muls@gmail.com')
+ftp.login('ftp', 'alain.muls@gmail.com')
 
 # Get All Files
 files = ['BRDC00IGS_R_20191000000_01D_MN.rnx.gz']
 
 # Print out the files
 for file in files:
-	print("Downloading..." + file)
-	ftp.retrbinary("RETR " + '/pub/gps/data/daily/2019/100/19p/'+file ,open("/tmp/" + file, 'wb').write)
+    print("Downloading..." + file)
+    ftp.retrbinary("RETR " + '/pub/gps/data/daily/2019/100/19p/' + file, open("/tmp/" + file, 'wb').write)
 
 ftp.close()
 
