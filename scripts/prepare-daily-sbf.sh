6c1deb3ef08618f375bbb6464628bda462210a6f
alainmuls <alain.muls@gmail.com>
2019-11-20 15:33:42 +0100

    split up automatic processing in a preparation phase to be followed by processing phase

--- scripts/prepare-daily-sbf.sh
+++ scripts/prepare-daily-sbf.sh
@@ -1,8 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-STDOY=105
-ENDOY=107
-YY=19
+usage()
+{
+    echo "usage: $0 -v pyenv-s startDOY -e endDOY -y YYYY -r RxType [-h]"
+}
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+		-v)	shift
+        	PYVENV=$1
+            ;;
+		-s)	shift
+        	STDOY=$1
+            ;;
+        -e) shift
+			ENDOY=$1
+            ;;
+    	-y) shift
+			YYYY=$1
+            ;;
+        -r) shift
+			RXTYPE=$1
+			;;
+        *)  usage
+            exit 1
+    esac
+    	shift
+done
+
+echo ${PYVENV}
+echo ${STDOY}
+echo ${ENDOY}
+echo ${YYYY}
+echo ${RXTYPE}
 
 # print DOY with leading zeros if needed
 function leading_zero()
@@ -13,7 +49,7 @@ function leading_zero()
 }
 
 # set the python virtual environment
-PROJECT_NAME=pyRTKLib
+PROJECT_NAME=${PYVENV}
 VIRTUAL_ENV=${WORKON_HOME}/${PROJECT_NAME}
 PYHOMEDIR=${PROJECT_HOME}/${PROJECT_NAME}
 # printf ${VIRTUAL_ENV}
@@ -49,9 +85,10 @@ printf '\nActivated python virtual environment '${PROJECT_HOME}/${PROJECT_NAME}'
 
 # ${PYHOMEDIR}/pySBFDaily.py -h
 
-for DOY in $(seq $STDOY $ENDOY)
+for curDOY in $(seq $STDOY $ENDOY)
 do
-	DOY=$(leading_zero ${DOY})
+	DOY=$(leading_zero ${curDOY})
+	YY=${YYYY: -2}
 	YYDOY=${YY}${DOY}
 	# printf ${YY}' '${DOY}' '${YYDOY}
 
@@ -61,29 +98,28 @@ do
 
 	printf '\nCreating daily file for '${YYDOY}'\n'
 	cd ${DIRRAW}
-	${NICE} ${PYSBFDAILY} --dir=${DIRRAW}
-
-	printf '\nDownloading OS files for '${YYDOY}'\n'
-	${NICE} ${PYFTPOSNAV} --rootdir=${DIRIGS} --year=20${YY} --doy=${DOY}
-
-	printf '\nCreating RINEX files for '${YYDOY}'\n'
-	if [ ${RXTYPE} = 'ASTX' ]
+	${NICE} ${PYSBFDAILY} --dir=${DIRRAW} --overwrite
+	exit_status=$?
+	if [ $exit_status -eq 0 ]
 	then
-		cd ${DIRRAW}
+		printf '\nDownloading OS files for '${YYDOY}'\n'
+		${NICE} ${PYFTPOSNAV} --rootdir=${DIRIGS} --year=20${YY} --doy=${DOY}
 
-		MARKER=SEPT
+		printf '\nCreating RINEX files for '${YYDOY}'\n'
+		if [ ${RXTYPE} = 'ASTX' ]
+		then
+			cd ${DIRRAW}
 
-		# the gnss and corresponding marker name lists
-		gnss=([0]='gal' [1]='gps' [2]='com')
-		gnssmark=([0]='GALI' [1]='GPSS' [2]='COMB')
+			MARKER=SEPT
 
+			# the gnss and corresponding marker name lists
+			gnss=([0]='gal' [1]='gps' [2]='com')
+			gnssmark=([0]='GALI' [1]='GPSS' [2]='COMB')
+		fi
 		for i in "${!gnss[@]}"
 		do
 			${NICE} ${PYCONVBIN} --dir=${DIRRAW} --file=${MARKER}${DOY}0.${YY}_ --rinexdir=${DIRRIN} --rinexver=R3 --binary=SBF --gnss=${gnss[i]} -n ${gnssmark[i]} ${DOY} ${YY}
 		done
-	elif [ ${RXTYPE} = 'BEGP' ]
-	then
-		MARKER=BEGP
 	fi
 done
 
