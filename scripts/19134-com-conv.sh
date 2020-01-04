fbd389cf2d03cb8b9324bf644edb90501e4ebaa5
alainmuls <alain.muls@gmail.com>
2020-01-04 15:29:01 +0100

    add script for GPS / COM day 19134

---
+++ scripts/19134-com-conv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyconvbin.py -d ~/RxTURP/BEGPIOS/ASTX/19134/ -f SEPT1340.19_ -b SBF -v R3 -r ~/RxTURP/BEGPIOS/ASTX/rinex/19134/ -n COMB 134 19 -o -g com -l INFO DEBUG
---
+++ scripts/19134-gps-conv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyconvbin.py -d ~/RxTURP/BEGPIOS/ASTX/19134/ -f SEPT1340.19_ -b SBF -v R3 -r ~/RxTURP/BEGPIOS/ASTX/rinex/19134/ -n GPSS 134 19 -o -g gps -l INFO DEBUG
