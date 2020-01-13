5a7a1682679a64fed0ed81f2eb4685e4808b2c87
alainmuls <alain.muls@gmail.com>
2020-01-13 10:43:44 +0100

    BEGP 19134

---
+++ scripts/19134-begp-conv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyconvbin.py -d ~/RxTURP/BEGPIOS/BEGP/19134/ -f BEGP1340.19_ -b SBF -v R3 -r ~/RxTURP/BEGPIOS/BEGP/rinex/19134/ -n BEGP 134 19 -o -g gal -l INFO DEBUG
---
+++ scripts/19134-begp-plot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyrtkplot.py -d /home/amuls/RxTURP/BEGPIOS/BEGP/rinex/19134/rtkp/gal/ -f BEGP1340-19O.pos -p -o -m 50.8440152778 4.3929283333 151.39179 -l INFO DEBUG
